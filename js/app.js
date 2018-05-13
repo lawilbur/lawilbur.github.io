473961a721a72ae413a5a6813482a4c9944e29ef
Alec Wilbur <lawilbur2@gmail.com>
2018-05-13 17:54:27 -0400

    created partials and started linking

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@ const app = angular.module("myApp", []);
 
 app.controller("MainController", ["$http", function($http){
 
+    this.includePath = 'partails/resume.html';
+    this.changeInclude = (path) => {
+        console.log("calling function");
+        this.includePath = 'partials/' + path + '.html';
+    };
 }]);
