e6d1c6f5da03f4153d6a2d178ffdd7ea08249304
Alec Wilbur <lawilbur2@gmail.com>
2018-05-13 19:31:02 -0400

    updated patials

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const app = angular.module("myApp", []);
 
 app.controller("MainController", ["$http", function($http){
 
-    this.includePath = 'partails/resume.html';
+    this.includePath = 'partials/about.html';
     this.changeInclude = (path) => {
         console.log("calling function");
         this.includePath = 'partials/' + path + '.html';
